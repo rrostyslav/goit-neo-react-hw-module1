b1b410611fd06166232923fb5528ca15f282845a
Rostyslav Rachynskyi <r.rostik2000@gmail.com>
2025-08-28 23:29:29 +0300

    feat: profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,1 +1,32 @@
-export default function Profile() {}
+import css from './Profile.module.css';
+
+export default function Profile({ name, tag, location, image, stats }) {
+  return (
+    <div className={css.container}>
+      <div className={css.userInfo}>
+        <img
+          src={image}
+          alt="User avatar"
+        />
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
